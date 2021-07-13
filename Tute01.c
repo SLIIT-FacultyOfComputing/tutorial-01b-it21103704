ca63e63e0fc14d7aca1127b4204c93501860ac68
IT21103704 <87373376+it21103704@users.noreply.github.com>
2021-07-13 14:57:06 +0000

    I have completed exercises 1

--- Tute01.c
+++ Tute01.c
@@ -2,10 +2,27 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
+#include <stdio.h>  //Adding Headerfiles
 
-int main() {
-  
+int main() 
+{
+
+  float sub1,sub2;
+  float total,average;  //Declaring Variables
+
+  printf("Enter Subject 1 Marks : \n");  //Getting Sub 1 Marks From User
+  scanf("%f", &sub1);
+
+  printf("Enter Subject 2 Marks : \n");  //Getting Sub 2 Marks From User
+  scanf("%f", &sub2);
+
+  total = sub1 + sub2;  //Calculate total
+  average = total/2.0;  //Calculate average
+
+  printf("Total marks = %.2f\n", total);  //Printing Total
+  printf("Average marks = %.2f\n", average);  //printing average
+
+
   return 0;
 }
 
