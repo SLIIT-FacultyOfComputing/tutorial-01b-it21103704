1593364ebdfa9ce56fffe3aca5885e3d812abaf2
IT21103704 <87373376+it21103704@users.noreply.github.com>
2021-07-14 03:39:03 +0000

    I have completed exersice 2

--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,29 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
+#include <stdio.h>  //Adding Headerfiles
 
-int main() {
+int main() 
+{
+
+  int Distance,Amount,a,b;   //Declare Variables
+
+  printf("Enter Distance : ");
+  scanf("%d", &Distance);   //Getting Distance Info From User
+
+   if(Distance <= 30)
+    {
+       a = Distance*50;
+    }
+
+    else if(Distance > 30)
+    {
+       b = Distance*40; 
+    }
+
+    Amount = a+b;  //Getting Final Amount
+
+
   
   return 0;
 }
